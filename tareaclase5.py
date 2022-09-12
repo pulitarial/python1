8f2d71517220f97325d4a65c68882cbe3721176b
pulitarial <82887116+pulitarial@users.noreply.github.com>
2022-09-11 21:43:40 -0300

    Add files via upload

---
+++ tareaclase5.py
@@ -0,0 +1,10 @@
+import tkinter
+
+ventana=tkinter.Tk()
+ventana.title("Mensajeria")
+ventana.geometry("250x400")
+ventana.resizable(0,0)
+ventana.config(bg="green", relief="groove", bd="5")
+
+
+ventana.mainloop()
